2dd405ee727d41b84e1785a0e672cf7476745407
Lautaro Montaña <joselautarom@gmail.com>
2024-10-17 08:50:12 -0300

    Creacion de segundo sout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!");
+        System.out.println("Tiene que verse en una nueva rama");
     }
 }
